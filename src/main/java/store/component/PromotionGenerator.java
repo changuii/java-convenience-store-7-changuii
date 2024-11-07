b596e9200e1c58d695411389e72c8ba1d115bc4c
changuii <rhljh7410@gmail.com>
2024-11-07 13:29:40 +0900

    feat : PromotionGenerator의 파일의 형식을 지키지 않은 경우를 검증하라
    - promotions.md 파일 형식에 대한 검증을 진행한다.

--- src/main/java/store/component/PromotionGenerator.java
+++ src/main/java/store/component/PromotionGenerator.java
@@ -1,6 +1,7 @@
 package store.component;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,10 +14,21 @@ import store.enums.StoreConfig;
 public class PromotionGenerator {
 
     public Map<String, Optional<Promotion>> generate(final List<String> promotionLines) {
+        validatePromotionLines(promotionLines);
         Map<String, Optional<Promotion>> promotions = initPromotions();
         registerPromotions(promotions, promotionLines);
 
         return promotions;
+    }
+
+    private void validatePromotionLines(final List<String> promotionLines){
+        promotionLines.forEach(this::validatePromotionLineFormat);
+    }
+
+    private void validatePromotionLineFormat(final String promotionLine){
+        if(!promotionLine.matches(StoreConfig.PROMOTION_VALUE_REGEX.getValue())){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FILE_FORMAT.getMessage());
+        }
     }
 
     private void registerPromotions(Map<String, Optional<Promotion>> promotions, List<String> promotionLines) {
@@ -32,18 +44,26 @@ public class PromotionGenerator {
     }
 
     private Promotion parsePromotion(final String promotion) {
-        String[] column = promotion.split(StoreConfig.TABLE_ROW_DELIMITER.getValue());
-        String promotionName = column[0];
-        int purchaseCount = parseInt(column[1]);
-        DateRange dateRange = parseDateRange(column[3], column[4]);
+        try {
+            String[] column = promotion.split(StoreConfig.TABLE_ROW_DELIMITER.getValue());
+            String promotionName = column[0];
+            int purchaseCount = parseInt(column[1]);
+            DateRange dateRange = parseDateRange(column[3], column[4]);
 
-        return Promotion.of(promotionName, purchaseCount, dateRange);
+            return Promotion.of(promotionName, purchaseCount, dateRange);
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FILE_FORMAT.getMessage());
+        }
     }
 
     private DateRange parseDateRange(final String startDate, final String endDate) {
-        LocalDate start = LocalDate.parse(startDate);
-        LocalDate end = LocalDate.parse(endDate);
-        return DateRange.of(start, end);
+        try {
+            LocalDate start = LocalDate.parse(startDate);
+            LocalDate end = LocalDate.parse(endDate);
+            return DateRange.of(start, end);
+        } catch (DateTimeParseException e){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FILE_FORMAT.getMessage());
+        }
     }
 
     private int parseInt(final String number) {
--- src/main/java/store/enums/StoreConfig.java
+++ src/main/java/store/enums/StoreConfig.java
@@ -4,7 +4,8 @@ public enum StoreConfig {
 
     PRODUCTS_VALUE_PATH("src/main/resources/products.md"),
     PROMOTIONS_VALUE_PATH("src/main/resources/promotions.md"),
-    TABLE_ROW_DELIMITER(",");
+    TABLE_ROW_DELIMITER(","),
+    PROMOTION_VALUE_REGEX("^([^,]+),(\\d+),(\\d+),(\\d{4}-\\d{2}-\\d{2}),(\\d{4}-\\d{2}-\\d{2})$");
 
 
     private final String value;

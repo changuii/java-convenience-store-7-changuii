22b0550ff74c42a70b9f9d95fbef933a63307d4f
changuii <rhljh7410@gmail.com>
2024-11-07 13:45:11 +0900

    refactor : Promotion 포맷 regex를 변경하라
    - \d대신 [0-9]를 사용하도록 변경하였다. \d가 0~9 외에 다른 숫자를 의미하는 문자를 포함할 수 있어서 변경하였다.

--- src/main/java/store/enums/StoreConfig.java
+++ src/main/java/store/enums/StoreConfig.java
@@ -5,7 +5,8 @@ public enum StoreConfig {
     PRODUCTS_VALUE_PATH("src/main/resources/products.md"),
     PROMOTIONS_VALUE_PATH("src/main/resources/promotions.md"),
     TABLE_ROW_DELIMITER(","),
-    PROMOTION_VALUE_REGEX("^([^,]+),(\\d+),(\\d+),(\\d{4}-\\d{2}-\\d{2}),(\\d{4}-\\d{2}-\\d{2})$");
+    PROMOTION_VALUE_REGEX("^([^,]+),([0-9]+),([0-9]+)(,[0-9]{4}-[0-9]{2}-[0-9]{2}){2}$"),
+    PRODUCT_VALUE_REGEX("^([^,])+,([0-9])+,([0-9])+,([^,])+$");
 
 
     private final String value;

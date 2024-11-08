06bd379ef352cb7ffb940358c6b974d2da7ecccc
changuii <rhljh7410@gmail.com>
2024-11-08 21:03:00 +0900

    feat : PurchaseProductDTO의 구매 수량이 0개 이하인 경우 예외를 발생시켜라

--- src/main/java/store/view/InputValidator.java
+++ src/main/java/store/view/InputValidator.java
@@ -14,6 +14,7 @@ public class InputValidator {
     private static final Pattern PURCHASE_PRODUCTS_FORMAT = Pattern.compile(PURCHASE_PRODUCTS_FORMAT_REGEX);
     private static final String ANSWER_FORMAT_REGEX = "^[YN]$";
     private static final Pattern ANSWER_FORMAT = Pattern.compile(ANSWER_FORMAT_REGEX);
+    private static final int PURCHASE_QUANTITY_MIN = 0;
 
 
     public void validatePurchaseProducts(String products) {
@@ -24,11 +25,20 @@ public class InputValidator {
 
     public void validatePurchaseProductDTOs(List<PurchaseProductDTO> purchaseProductDTOs) {
         validateDuplicationPurchaseProductDTOs(purchaseProductDTOs);
+        purchaseProductDTOs.forEach(purchaseProductDTO -> {
+            validatePurchaseQuantityLessThanMin(purchaseProductDTO);
+        });
     }
 
     private void validateDuplicationPurchaseProductDTOs(List<PurchaseProductDTO> purchaseProductDTOs) {
         if (purchaseProductDTOs.stream().map(PurchaseProductDTO::getName).distinct().count()
                 != purchaseProductDTOs.size()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.getMessage());
+        }
+    }
+
+    private void validatePurchaseQuantityLessThanMin(PurchaseProductDTO purchaseProductDTO){
+        if(purchaseProductDTO.getQuantity() <= PURCHASE_QUANTITY_MIN){
             throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.getMessage());
         }
     }

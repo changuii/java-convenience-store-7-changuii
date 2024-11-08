5300389d63a8d4ada21152a82f571f3e3c8f2849
changuii <rhljh7410@gmail.com>
2024-11-08 22:57:03 +0900

    test : Promotion의 단위 테스트를 작성하라
    - isValidNow 메서드가 올바로 작동하는지 테스트하였다.

---
+++ src/test/java/store/domain/PromotionTest.java
@@ -0,0 +1,21 @@
+package store.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PromotionTest {
+
+    @DisplayName("매개변수로 들어온 LocalDate가 DateRange 범위 이내라면 true 아니라면 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2023-11-01:true", "2023-11-30:true", "2023-10-31:false", "2023-12-01:false"}, delimiter = ':')
+    void isValidNow(LocalDate today, boolean expected) {
+        DateRange dateRange = DateRange.of(LocalDate.of(2023, 11, 01), LocalDate.of(2023, 11, 30));
+        Promotion promotion = Promotion.of("1+1", BuyGet.of(1,1), dateRange);
+
+        assertThat(promotion.isValidNow(today)).isEqualTo(expected);
+    }
+}

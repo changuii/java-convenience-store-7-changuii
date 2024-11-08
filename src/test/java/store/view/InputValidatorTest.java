bb70358776d75a52652d217f41ddb012bcf227fe
changuii <rhljh7410@gmail.com>
2024-11-08 20:50:49 +0900

    test : PurchaseProduct List에 대한 검증 로직을 테스트하라

--- src/test/java/store/view/InputValidatorTest.java
+++ src/test/java/store/view/InputValidatorTest.java
@@ -2,9 +2,15 @@ package store.view;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import store.dto.PurchaseProductDTO;
 import store.enums.ErrorMessage;
 
 public class InputValidatorTest {
@@ -15,13 +21,26 @@ public class InputValidatorTest {
         this.inputValidator = new InputValidator();
     }
 
+    private static Stream<List<PurchaseProductDTO>> providePurchaseProductDTO() {
+        return Stream.of(
+                List.of(PurchaseProductDTO.of("사이다", 2), PurchaseProductDTO.of("사이다", 2)),
+                List.of(PurchaseProductDTO.of("김치", 6), PurchaseProductDTO.of("김치", 2)),
+                List.of(PurchaseProductDTO.of("밤티라미수", 6), PurchaseProductDTO.of("김치", 2),
+                        PurchaseProductDTO.of("밤티라미수", 5))
+        );
+    }
+
     @ParameterizedTest
     @EmptySource
     @ValueSource(strings = {"김치-1]", "[김치-1", "[김치-]", "[김치1]", "[-1]", "[김치-1],", "[김치-1],[감자-]"})
     void 잘못된_형식의_상품들이_입력으로_들어오면_예외가_발생한다(String products) {
-        assertThatThrownBy(() -> inputValidator.validatePurchaseProducts(products))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.INVALID_FORMAT.getMessage());
+        assertThatException(inputValidator::validatePurchaseProducts, products);
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePurchaseProductDTO")
+    void 같은_상품들이_입력으로_들어오면_예외가_발생한다(List<PurchaseProductDTO> inputs) {
+        assertThatException(inputValidator::validatePurchaseProductDTOs, inputs);
     }
 
     @ParameterizedTest
@@ -34,10 +53,10 @@ public class InputValidatorTest {
     @EmptySource
     @ValueSource(strings = {"Y ", "N ", " Y", " N", "A", "[", "]", "-", "W", "0", "q", "n", "y"})
     void Y혹은N_외의_입력이_들어오면_예외가_발생한다(String answer) {
-        assertThatThrownBy(() -> inputValidator.validateAnswer(answer))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.INVALID_FORMAT.getMessage());
+        assertThatException(inputValidator::validateAnswer, answer);
     }
+
+
 
     @ParameterizedTest
     @ValueSource(strings = {"Y", "N"})
@@ -45,4 +64,9 @@ public class InputValidatorTest {
         inputValidator.validateAnswer(answer);
     }
 
+    private <T> void assertThatException(Consumer<T> logic, T data){
+        assertThatThrownBy(() -> logic.accept(data))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_FORMAT.getMessage());
+    }
 }

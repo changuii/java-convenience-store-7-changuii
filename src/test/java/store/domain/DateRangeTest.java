3c7e2f2d2cc9d6879e7a61948e934fa9d4b715ff
changuii <rhljh7410@gmail.com>
2024-11-08 22:14:30 +0900

    test : DateRange의 단위 테스트를 작성하라

---
+++ src/test/java/store/domain/DateRangeTest.java
@@ -0,0 +1,24 @@
+package store.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class DateRangeTest {
+
+
+    @DisplayName("현재 날짜가 startDate 이상, endDate 이하라면, True 아니라면 False를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2023-11-01:true", "2023-11-30:true", "2023-10-31:false", "2023-12-01:false"}, delimiter = ':')
+    void isInRangeNow(String input, boolean expected){
+        LocalDate today = LocalDate.parse(input);
+        LocalDate start = LocalDate.of(2023, 11, 01);
+        LocalDate end = LocalDate.of(2023, 11, 30);
+        DateRange dateRange = DateRange.of(start, end);
+
+        assertThat(dateRange.isInRangeNow(today)).isEqualTo(expected);
+    }
+}

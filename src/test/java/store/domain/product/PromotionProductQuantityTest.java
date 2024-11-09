bae80c7e347116f447658cbaf9ee3290e0539916
changuii <rhljh7410@gmail.com>
2024-11-09 22:42:12 +0900

    test : PromotionProductQuantity의 isMatchProduct를 테스트하라

--- src/test/java/store/domain/product/PromotionProductQuantityTest.java
+++ src/test/java/store/domain/product/PromotionProductQuantityTest.java
@@ -8,23 +8,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import store.Constants;
+import store.domain.PurchaseProduct;
 
 public class PromotionProductQuantityTest {
+    private final DateRange dateRange;
+    private final BuyGet buyGet;
     private Promotion promotion;
+
+    public PromotionProductQuantityTest() {
+        dateRange = DateRange.of(LocalDate.of(2023, 11, 01), LocalDate.of(2023, 11, 30));
+        buyGet = BuyGet.of(1, 1);
+    }
 
     @BeforeEach
     void init() {
-        DateRange dateRange = DateRange.of(LocalDate.of(2023, 11, 01), LocalDate.of(2023, 11, 30));
-        promotion = Promotion.of(Constants.PROMOTION_NAME, BuyGet.of(1, 1), dateRange);
+        promotion = Promotion.of(Constants.PROMOTION_NAME, buyGet, dateRange);
     }
 
 
     @DisplayName("매개변수로_들어온_ProductQuantity와_재고를_합산해_재고가_충분히_있는지_검증한다")
     @ParameterizedTest
     @CsvSource(value = {"1:1:2:true", "2:2:4:true", "1:0:2:false", "2:1:4:false"}, delimiter = ':')
-    void isLessThanQuantityTest(int promotionQuantity, int quantity, int purchaseQuantity, boolean expected) {
+    void isLessThanQuantityTest(final int promotionQuantity, final int quantity, final int purchaseQuantity,
+                                final boolean expected) {
         ProductQuantity productQuantity = ProductQuantity.of(Constants.PRODUCT_NAME, quantity);
-        PromotionProductQuantity promotionProductQuantity = PromotionProductQuantity.of(Constants.PRODUCT_NAME, promotionQuantity,
+        PromotionProductQuantity promotionProductQuantity = PromotionProductQuantity.of(Constants.PRODUCT_NAME,
+                promotionQuantity,
                 promotion);
 
         boolean actual = promotionProductQuantity.isLessThanQuantity(purchaseQuantity, productQuantity);
@@ -42,4 +51,14 @@ public class PromotionProductQuantityTest {
         assertThat(promotionQuantity.isValidToday(today)).isEqualTo(expected);
     }
 
+    @DisplayName("purchaseProduct와 이름이 같다면 true를 반환하고 아니라면 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"PRODUCT:true", "감자:false", "치킨:false"}, delimiter = ':')
+    void isMatchProduct(final String purchaseProductName, final boolean expected) {
+        PromotionProductQuantity promotionProductQuantity = PromotionProductQuantity.of(Constants.PRODUCT_NAME,
+                Constants.PRODUCT_QUANTITY_VALUE, promotion);
+        PurchaseProduct purchaseProduct = PurchaseProduct.of(purchaseProductName, Constants.PRODUCT_QUANTITY_VALUE);
+
+        assertThat(promotionProductQuantity.isMatchProduct(purchaseProduct)).isEqualTo(expected);
+    }
 }

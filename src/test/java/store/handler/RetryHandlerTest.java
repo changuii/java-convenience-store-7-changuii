6802c56d8bd06ebcaecbd0588f4a86e2692731a2
changuii <rhljh7410@gmail.com>
2024-11-09 11:10:57 +0900

    test : RetryHandler의 단위 테스트를 작성하라
    - MockOutputView : 혹시 모를 OutputView의 결합을 제거해서 RetryHandler만 테스트할 수 있도록 만든 테스트 대역
    - Counter : 재시도 횟수를 카운트하는 클래스

---
+++ src/test/java/store/handler/RetryHandlerTest.java
@@ -0,0 +1,83 @@
+package store.handler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import store.view.OutputView;
+
+public class RetryHandlerTest {
+
+    private final RetryHandler retryHandler;
+    private final MockOutputView mockOutputView;
+
+
+    public RetryHandlerTest() {
+        retryHandler = new RetryHandler();
+        mockOutputView = new MockOutputView();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 20, 50, 100})
+    void 예외가_발생하지_않을때_까지_반복_실행한다(int N) {
+        Counter counter = new Counter(N);
+
+        int actual = retryHandler.retryUntilNotException(counter::countFromUntilN, mockOutputView);
+
+        assertThat(actual).isEqualTo(N);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 20, 30, 100})
+    void False가_발생하지_않을때_까지_반복_실행한다(int N){
+        Counter counter = new Counter(N);
+
+        retryHandler.retryUntilFalse(counter::countFromUntilN, counter::isValid, N);
+
+        assertThat(counter.getCount()).isEqualTo(N);
+    }
+
+
+    private class Counter {
+        private int count;
+        private int N;
+
+        public Counter(int N) {
+            this.count = 0;
+            this.N = N;
+        }
+
+        public int countFromUntilN() {
+            count++;
+            if (count < N) {
+                throw new IllegalArgumentException();
+            }
+            return count;
+        }
+
+        public void countFromUntilN(int garbage){
+            count++;
+        }
+
+        public boolean isValid(){
+            return count == N;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+
+    }
+
+
+    private class MockOutputView extends OutputView {
+
+        @Override
+        public void printErrorMessage(IllegalArgumentException e) {
+            System.out.println("예외 발생");
+        }
+    }
+
+
+}

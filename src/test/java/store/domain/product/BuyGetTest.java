1d6b1a1d66f92993cd563ab7add0e6461bc1d172
changuii <rhljh7410@gmail.com>
2024-11-10 14:12:14 +0900

    test : BuyGet의 로직에 대해 단위 테스트를 작성하라

---
+++ src/test/java/store/domain/product/BuyGetTest.java
@@ -0,0 +1,57 @@
+package store.domain.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BuyGetTest {
+
+
+
+    @DisplayName("주어진 수량에 대해 무료로 제공할 수량을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"5:2:1:2", "7:3:1:2", "2:1:1:1", "3:1:1:2"}, delimiter = ':')
+    void calculateFreeQuantity(final int quantity, final int buy, final int get, final int expected){
+        BuyGet buyGet = BuyGet.of(buy, get);
+
+        int actual = buyGet.calculateFreeQuantity(quantity);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("주어진 수량에 대해 필요한 프로모션 수량을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"5:2:1:6", "7:3:1:8", "2:1:1:2", "3:1:1:4"}, delimiter = ':')
+    void calculateQuantityCondition(final int quantity, final int buy, final int get, final int expected){
+        BuyGet buyGet = BuyGet.of(buy, get);
+
+        int actual = buyGet.calculateQuantityCondition(quantity);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("주어진 수량에 대해 프로모션을 적용 가능한 수량을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"10:2:1:9", "7:3:1:4", "2:1:1:2", "3:1:1:2", "7:2:1:6"}, delimiter = ':')
+    void calculateApplicableQuantity(final int quantity, final int buy, final int get, final int expected){
+        BuyGet buyGet = BuyGet.of(buy, get);
+
+        int actual = buyGet.calculateApplicableQuantity(quantity);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("주어진 수량에 대해 적용 가능한 무료 수량을 계산하여 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"5:2:1:1", "7:3:1:1", "2:1:1:1", "3:1:1:1", "7:2:1:2"}, delimiter = ':')
+    void calculateApplicableFreeQuantity(final int quantity, final int buy, final int get, final int expected){
+        BuyGet buyGet = BuyGet.of(buy, get);
+
+        int actual = buyGet.calculateApplicableFreeQuantity(quantity);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}

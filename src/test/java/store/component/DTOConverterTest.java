850f1a8844c20ee7dae61019de472de5adf86837
changuii <rhljh7410@gmail.com>
2024-11-11 16:36:57 +0900

    test : DTOConverter에 대한 테스트를 작성하라
    - Bill을 DTO로 변환하는 메서드 및 PurchaseProductDTO를 도메인으로 변환하는 메서드 테스트를 작성하였다.

--- src/test/java/store/component/DTOConverterTest.java
+++ src/test/java/store/component/DTOConverterTest.java
@@ -7,9 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import store.Constants;
+import store.domain.Bill;
+import store.domain.CompletedPurchaseHistory;
+import store.domain.PurchaseProduct;
 import store.domain.product.BuyGet;
 import store.domain.product.DateRange;
 import store.domain.product.ProductInfo;
@@ -17,8 +21,10 @@ import store.domain.ProductInventory;
 import store.domain.product.ProductQuantity;
 import store.domain.product.Promotion;
 import store.domain.product.PromotionProductQuantity;
+import store.dto.BillDTO;
 import store.dto.ProductDTO;
 import store.dto.ProductInventoryDTO;
+import store.dto.PurchaseProductDTO;
 
 public class DTOConverterTest {
     private final DTOConverter dtoConverter;
@@ -54,6 +60,33 @@ public class DTOConverterTest {
 
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
+
+    @DisplayName("PurchaseProductDTO를 PurchaseProduct로 변환 테스트")
+    @Test
+    void convertPurchaseProducts() {
+        List<PurchaseProductDTO> purchaseProductDTOs = List.of(
+                PurchaseProductDTO.of(Constants.PROMOTION_NAME, Constants.PRODUCT_QUANTITY_VALUE));
+        List<PurchaseProduct> expected = List.of(
+                PurchaseProduct.of(Constants.PROMOTION_NAME, Constants.PRODUCT_QUANTITY_VALUE));
+
+        List<PurchaseProduct> actual = dtoConverter.convertPurchaseProducts(purchaseProductDTOs);
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @DisplayName("Bill을 BillDTO로 변환 테스트")
+    @Test
+    void convertBillDTO() {
+        Bill bill = Bill.from(List.of(CompletedPurchaseHistory.of(Constants.PRODUCT_NAME, 6000, 2, 4, 2)));
+        BillDTO expected = BillDTO.of(
+                List.of(ProductDTO.of(Constants.PRODUCT_NAME, 1000, 8, "")),
+                List.of(ProductDTO.of(Constants.PRODUCT_NAME, 0, 2, "")), 8000, 2000, 0, 6000, 8);
+
+        BillDTO actual = dtoConverter.convertBillDTO(bill);
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
 
     private ProductInventory createProductInventory(final String productName, final int productPrice,
                                                     final int quantity, final int promotionQuantity) {
